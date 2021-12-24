0c136e4824a39161395c13fc51538a7b52920c5b
Thanhnhan1301 <huynh235@gmail.com>
2021-12-24 19:08:21 +0700

    fix 1

--- components/common/Header/index.jsx
+++ components/common/Header/index.jsx
@@ -11,30 +11,28 @@ export default function Header() {
             <div className='bg-gray-900 p-5 flex justify-between items-center'>
                 <div>
                     <Link href='/' passHref={true}>
-                        <a>
-                            <div className='rounded-sm flex items-center gap-[20px]'>
-                                <Image
-                                    src='/logo.jpg'
-                                    alt='logo'
-                                    layout='fixed'
-                                    width='45px'
-                                    height='45px'
-                                    className='rounded-sm'
-                                />
-                                <div className='text-white text-[14px]'>
-                                    <span className='block leading-5'>
-                                        Công ty Luật TNHH
-                                    </span>
-                                    <span className='block leading-5'>
-                                        Luật sư Hà
-                                    </span>
-                                </div>
+                        <div className='rounded-sm flex items-center gap-[24px]'>
+                            <Image
+                                src='/logo.jpg'
+                                alt='logo'
+                                layout='fixed'
+                                width='50px'
+                                height='50px'
+                                className='rounded-sm'
+                            />
+                            <div className='text-white'>
+                                <span className='block leading-6'>
+                                    Công ty Luật TNHH
+                                </span>
+                                <span className='block leading-6'>
+                                    Luật sư Hà
+                                </span>
                             </div>
-                        </a>
+                        </div>
                     </Link>
                 </div>
                 <div
-                    className='text-white p-[6px] border border-white rounded-sm opacity-90 cursor-pointer'
+                    className='text-white p-2 text-lg border border-white rounded-sm opacity-90 cursor-pointer'
                     onClick={() => setShowNavLink(!showNavLink)}
                 >
                     <FaBars />
