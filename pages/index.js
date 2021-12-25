00ec983e3b2b0a2aa153c094a333df5ac8250d80
Thanhnhan1301 <huynh235@gmail.com>
2021-12-25 11:45:18 +0700

    add router 1.2

--- pages/index.js
+++ pages/index.js
@@ -27,14 +27,13 @@ export default function Home() {
             <div className='p-12 lg:flex lg:flex-wrap lg:justify-between'>
                 {data.map((item) => {
                     return (
-                        <Link href={item.path}>
+                        <Link href={item.path} key={item.path}>
                             <div
                                 className='
                                     group w-full bg-gray-300 rounded-md 
                                     cursor-pointer shadow-lg mb-10
                                     lg:w-[48%]
                                 '
-                                key={item.path}
                             >
                                 <div className='h-[4px] bg-blue-500 rounded-t-md w-0 group-hover:w-full transition-[width]' />
                                 <h2 className='leading-6 text-center px-3 h-[100px] font-bold flex justify-center items-center'>
